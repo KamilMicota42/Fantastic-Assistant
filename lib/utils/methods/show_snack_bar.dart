6337f0c91133430ff0b9895c9a293409c592b73a
Kamil <kamil.micota@goodsoft.pl>
2023-10-14 20:17:59 +0100

    [FA-24] - Stylowanie snack bar'u

--- lib/utils/methods/show_snack_bar.dart
+++ lib/utils/methods/show_snack_bar.dart
@@ -1,9 +1,19 @@
+import 'package:fantastic_assistant/utils/const/app_colors.dart';
+import 'package:fantastic_assistant/utils/global_var/default_text_theme.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(text),
+      content: Text(
+        text,
+        style: DefaultTextTheme.titilliumWebRegular16(context)!.copyWith(
+          color: AppColors.white,
+        ),
+      ),
+      backgroundColor: AppColors.darkerGrey,
+      behavior: SnackBarBehavior.floating,
+      elevation: 6,
     ),
   );
 }
