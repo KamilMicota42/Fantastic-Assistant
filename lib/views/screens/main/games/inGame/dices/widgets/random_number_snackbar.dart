b70d4f43237a8d94e3e8b243eca255c5a0269887
“KamilMicota42” <kamilmicota42@gmail.com>
2024-05-12 18:28:12 +0200

    dices images under the roll simulator

--- lib/views/screens/main/games/inGame/dices/widgets/random_number_snackbar.dart
+++ lib/views/screens/main/games/inGame/dices/widgets/random_number_snackbar.dart
@@ -6,8 +6,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../../../../../../../utils/const/app_colors.dart';
 import '../../../../../../../widgets/buttons/default_button.dart';
 
-void showRandomNumberRoll(
-    BuildContext context, List<AnimatedText> randoms, String diceName) async {
+void showRandomNumberRoll(BuildContext context, List<AnimatedText> randoms, String diceName) async {
   context.mounted
       ? showDialog(
           context: context,
@@ -20,38 +19,45 @@ void showRandomNumberRoll(
       : null;
 }
 
-Widget _randomNumberRollPopup(
-  BuildContext context,
-  final List<AnimatedText> randoms,
-  String diceName,
-) {
+Widget _randomNumberRollPopup(BuildContext context, final List<AnimatedText> randoms, String diceName) {
   return AlertDialog(
     backgroundColor: AppColors.darkerGrey,
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Rolling ${diceName.toUpperCase()}',
-          style: DefaultTextTheme.titilliumWebBold20(context),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(width: 4),
-        SvgPicture.asset(
-          'assets/images/$diceName.svg',
-          width: 20,
-          height: 20,
-          fit: BoxFit.fill,
-        ),
-      ],
+    title: Text(
+      'Rolling ${diceName.toUpperCase()}',
+      style: DefaultTextTheme.titilliumWebBold20(context),
+      textAlign: TextAlign.center,
     ),
     content: Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        AnimatedTextKit(
-          repeatForever: false,
-          isRepeatingAnimation: true,
-          animatedTexts: randoms,
-          pause: const Duration(milliseconds: 0),
+        Stack(
+          children: [
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Opacity(
+                opacity: 0.10,
+                child: SvgPicture.asset(
+                  'assets/images/$diceName.svg',
+                  width: 20,
+                  height: 20,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Center(
+                child: AnimatedTextKit(
+                  repeatForever: false,
+                  isRepeatingAnimation: true,
+                  animatedTexts: randoms,
+                  pause: const Duration(milliseconds: 0),
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     ),
@@ -61,8 +67,7 @@ Widget _randomNumberRollPopup(
         function: () {
           Navigator.of(context).pop();
         },
-        textStyle: DefaultTextTheme.titilliumWebBold16(context)!
-            .copyWith(color: AppColors.lighterGrey),
+        textStyle: DefaultTextTheme.titilliumWebBold16(context)!.copyWith(color: AppColors.lighterGrey),
       ),
     ],
   );
